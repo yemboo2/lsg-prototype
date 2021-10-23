8cf5ed73eb8759eba4122ea367ad2d04fae5c533
Markus Ehringer <markus.ehringer@sbg.at>
2021-10-23 14:24:49 +0200

    feat(storybook): setup storybook + add exemplarly component: Button

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,50 @@
+import './button.css';
+
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * NOTE: Only purpose is to show storybook functionaly.
+ * TODO: remove when other components + stories have been introduced
+ */
+const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  onClick,
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
