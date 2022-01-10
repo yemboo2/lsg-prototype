5ef164bb8e13a4ba58b2183589381ace37164588
Markus Ehringer <markus.ehringer@sbg.at>
2022-01-10 10:20:09 +0100

    feat(proxy-lambda): add start-zuschuss credential set

--- src/proxy.js
+++ src/proxy.js
@@ -10,14 +10,20 @@ exports.handler = (event, context, callback) => {
   const headers = request.headers;
 
   // Configure authentication
-  const authUser = 'lsg'; // TODO:
+  const authUser = 'lsg';
   const authPass = 'lsg2021';
+  const authUser2 = 'Start';
+  const authPass2 = '!Zuschuss';
 
   // Construct the Basic Auth string
   const authString = 'Basic ' + new Buffer(authUser + ':' + authPass).toString('base64');
+  const authString2 = 'Basic ' + new Buffer(authUser2 + ':' + authPass2).toString('base64');
 
   // Require Basic authentication
-  if (typeof headers.authorization == 'undefined' || headers.authorization[0].value != authString) {
+  if (
+    typeof headers.authorization == 'undefined' ||
+    !(headers.authorization[0].value == authString || headers.authorization[0].value == authString2)
+  ) {
     const body = 'Unauthorized';
     const response = {
       status: '401',
